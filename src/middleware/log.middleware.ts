8ca674eb7180d0f56e1e59aa35119c1215223ff3
Boryhit <mateusvog@gmail.com>
2025-10-16 21:08:30 -0300

    depois do npm run build

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -2,7 +2,6 @@ import morgan from 'morgan';
 import fs from 'fs';
 import path from 'path';
 
-// ✅ Usa o __dirname nativo do CommonJS (sem import.meta)
 const fileLog = fs.createWriteStream(
   path.join(__dirname, '..', 'storage', 'access.log'),
   { flags: 'a' }
